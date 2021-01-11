34318ea67d13263ed0e64e24e3ddae93b3ce2095
tometseslive <64502294+tometseslive@users.noreply.github.com>
2021-01-11 14:05:15 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+function clock(){
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  if(m < 10) m = "0"+m;
+  if(s < 10) s = "0"+s;
+  document.getElementById("clock").innerHTML = `${h}:${m}:${s}`
+}
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function init(){
+  console.log("initialized!")
+  setInterval(clock, 1000);
+}
+
+function hide_beta(){
+  document.getElementById("aa_notice").style.display = "none";
+}
